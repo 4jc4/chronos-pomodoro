e7a8bed18fd717bc92d679806848a64715d7bcb7
Cardoso <ajca.dev@gmail.com>
2025-12-18 07:16:05 -0400

    chore: add app

---
+++ src/components/Container.tsx
@@ -0,0 +1,29 @@
+export function Container() {
+  return (
+    <>
+      <div className="container">
+        <div className="content">
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
+    </>
+  );
+}
